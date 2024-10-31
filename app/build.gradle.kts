9f7970da5294de40d862798f9b11c3eabee954e5
Đặng Việt Anh <2151170558@e.tlu.edu.vn>
2024-11-01 01:32:16 +0700

    commit changes Random_Exam

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 }
 
 dependencies {
-    implementation ("com.google.android.gms:play-services-base:18.0.1")
+    implementation("com.google.android.gms:play-services-base:18.0.1")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -40,12 +40,13 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation ("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation(platform("com.squareup.okhttp3:okhttp-bom:4.12.0"))
-    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("com.squareup.okhttp3:okhttp")
     implementation("com.squareup.okhttp3:logging-interceptor")
-    implementation ("com.google.android.gms:play-services-base:18.0.1")
+    implementation("com.google.android.gms:play-services-base:18.0.1")
 
 }
+
